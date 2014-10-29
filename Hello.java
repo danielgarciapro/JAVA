5eb5ce6551be9122f07873e8a0a5880b7f497729
Dani Garcia <danielgm2626@gmail.com>
2014-10-29 12:18:11 +0100

    cambios en hello.java

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public class Hello{
 		System.out.println("Hola mundo");
 		System.out.println("Hola a todos");
 		System.out.println("He creado una rama nueva");
+		System.out.println("Hola que ase");
 	}
 }
