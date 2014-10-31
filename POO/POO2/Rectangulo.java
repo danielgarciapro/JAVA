ad727272350e69007a2b2065c4dc6e79bf6f7625
Daniel Garcia <danielgm2626@gmail.com>
2014-10-31 10:13:17 +0100

    añado P002 con cambios

--- POO/POO2/Rectangulo.java
+++ POO/POO2/Rectangulo.java
@@ -8,7 +8,7 @@ public class Rectangulo{
 		return this.ancho;
 	}
 	public int getArea(){ //metodo que devuelve el area (entre llaves)
-		return this.ancho*alto;
+		return this.ancho*this.alto;
 	}
 	public void setAlto(int al){ //metodo de tipo mutante que cambia el valor del alto
 		this.alto=al;
@@ -17,12 +17,13 @@ public class Rectangulo{
 		this.alto=an;
 	}
 }
-class TestRectangulo{
+class TestRectangulo{ //podemos omitir esta clase, probar con la main y luego comentarla
 	public static void main (String[] args){
 		Rectangulo rec1 = new Rectangulo(); //crea un objeto tipo rectangulo
 		System.out.println("El alto del rectangulo es: "+rec1.getAlto());//llamamos a la clase
 		System.out.println("El ancho del rectangulo es: "+rec1.getAncho());//llamamos a la clase
 		System.out.println("El area del rectangulo es: "+rec1.getArea());//llamamos a la clase
+		/* rec1.setAlto(12); rec1.setAncho(15); de esta forma puedo darle valor a los atributos */
 		Rectangulo rec2 = new Rectangulo(); //crea un objeto tipo rectangulo
 		rec2.setAlto(10); //cambio el alto a 10
 		System.out.println("El nuevo alto del rectangulo es: "+rec2.getAlto());//llamamos a la clase
