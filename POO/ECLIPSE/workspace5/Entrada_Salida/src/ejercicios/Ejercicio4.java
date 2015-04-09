52fba3c388deee88aafdef1f8fb364fe54d82bb0
Daniel Garcia <danielgm2626@gmail.com>
2015-04-09 15:50:40 +0200

    ejercicios ent sal actualizados

--- POO/ECLIPSE/workspace5/Entrada_Salida/src/ejercicios/Ejercicio4.java
+++ POO/ECLIPSE/workspace5/Entrada_Salida/src/ejercicios/Ejercicio4.java
@@ -1,19 +1,30 @@
 package ejercicios;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Ejercicio4 {
 
 	public static void main(String[] args) {
 		//creamos el descrptor del fichero
 		File inFile = new File("/home/matinal/Escritorio/EjerciciosEntrada_Salida/libros_copia.csv");
-		List<Libro> listaLibros = new ArrayList<Libro>();
+		List<Libro> listaLibros = new ArrayList<Libro>(); //lista total de libros
+		//Colecciones Set, no se pueden repetir los objetos que contiene
+		Set<Libro> listaDiezLibros = new HashSet<Libro>();
 		//creamos el flujo de entrada
 		try (BufferedReader in = new BufferedReader(
 					new FileReader(inFile));){
@@ -32,11 +43,51 @@ public class Ejercicio4 {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		System.out.println(listaLibros);
+		//System.out.println(listaLibros);
+		//sacamos 10 libros al azar y añadirlos a la nueva lista
+		do {
+			//posicion aleatoria
+			int posicionLibroLista = (int) (Math.random()*listaLibros.size());
+			listaDiezLibros.add(listaLibros.get(posicionLibroLista));
+		} while (listaDiezLibros.size() < 10);
+		System.out.println(listaDiezLibros);
+		
+		//vamos a crear el fichero de salida
+		File outFile = new File("/home/matinal/Escritorio/EjerciciosEntrada_Salida/ejercicio4.dat");
+		//creamos el stream de salida
+		try (ObjectOutputStream out = new ObjectOutputStream(
+				new BufferedOutputStream(new FileOutputStream(outFile)));){
+			
+			out.writeObject(listaDiezLibros);
+			
+		} catch (IOException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		//vamos a leer los datos del fichero libros.dat
+				//
+		try (ObjectInputStream in = new ObjectInputStream(
+				new BufferedInputStream(new FileInputStream(outFile)));){
+					
+			@SuppressWarnings("unchecked") //para eliminar el warning
+			Set<Libro> objetoLeido = (Set<Libro>) in.readObject();
+			System.out.println(objetoLeido);
+					
+		} catch (IOException | ClassNotFoundException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		
 	}
 
 }
-class Libro{
+class Libro implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String titulo;
 	private String autor;
 	private String isbn;
@@ -52,6 +103,36 @@ class Libro{
 	@Override
 	public String toString() {
 		return "Libro [titulo=" + titulo + ", autor=" + autor + ", isbn="
-				+ isbn + "]";
+				+ isbn + "]\n";
 	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((isbn == null) ? 0 : isbn.hashCode());
+		return result;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Libro other = (Libro) obj;
+		if (isbn == null) {
+			if (other.isbn != null)
+				return false;
+		} else if (!isbn.equals(other.isbn))
+			return false;
+		return true;
+	}
+	
 }
---
+++ POO/ECLIPSE/workspace5/Entrada_Salida/src/ejercicios/Ejercicio6.java
@@ -0,0 +1,41 @@
+package ejercicios;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Ejercicio6 {
+	public static void main(String[] args) {
+		String path = "/home/matinal/Escritorio/EjerciciosEntrada_Salida/";
+		File inFile = new File(path+"Palabras.java");
+		
+		File outFile1 = new File(path+"Palabras1.java");
+		File outFile2 = new File(path+"Palabras2.java");
+		
+		//creamos un array de tipo char
+		char[] parte1 = new char [300]; //añadimos la primera parte del archivo
+		char[] parte2 = new char [(int) (inFile.length()-parte1.length)]; //tamaño del segundo array es el tamaño del archivo menos el tamaño del primer array
+		//stream de lectura y escritura
+		try (BufferedReader in = new BufferedReader(
+				new FileReader(inFile));
+				BufferedWriter out1 = new BufferedWriter(
+						new FileWriter(outFile1))){
+			//añadir los primeros 300 bytes a la parte1
+			in.read(parte1);
+			//el resto a parte2
+			in.read(parte2, parte1.length, parte2.length);
+			
+			//escribimos en salida1
+			out1.write(parte1); out1.flush(); //hacemos el volcado con el flush
+			
+			
+		} catch (IOException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+}
